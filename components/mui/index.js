2da0101f8a630971aaf17972865e8480002606d4
Andrew Mottersehead <andrew@frenchmottershead.com>
2023-12-05 18:40:01 +0000

    MUI clean up

--- components/mui/index.js
+++ components/mui/index.js
@@ -1,9 +1,13 @@
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import Container from '@mui/material/Container';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export {
-    Button,
-    EditIcon,
-    Container
+    Button, EditIcon, Container, AppBar, Box, IconButton, MenuIcon, Toolbar, Typography,
 };
