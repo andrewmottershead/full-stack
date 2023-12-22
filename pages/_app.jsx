ec050493a4845784b9fe303c9af904815a6d4d95
Andrew Mottersehead <andrew@frenchmottershead.com>
2023-12-22 16:02:22 +0000

    Listing flawed

---
+++ pages/_app.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { UserProvider } from "@auth0/nextjs-auth0/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+
+// import { UIProvider } from "@/components/contexts/UI.context";
+
+import "@/styles/globals.css";
+import theme from "@/lib/theme";
+
+export default function App({ Component, pageProps }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            suspense: true,
+            staleTime: 1000 * 10,
+            retry: process.env.NODE_ENV === "production",
+            refetchOnWindowFocus: process.env.NODE_ENV === "production",
+          },
+        },
+      })
+  );
+
+  return (
+    <>
+      <CssBaseline />
+      {/* <UserProvider> */}
+        <ThemeProvider theme={theme}>
+          <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools />
+            <Hydrate state={pageProps.dehydratedState}>
+              {/* <UIProvider> */}
+                <Component {...pageProps} />
+              {/* </UIProvider> */}
+            </Hydrate>
+          </QueryClientProvider>
+        </ThemeProvider>
+      {/* </UserProvider> */}
+    </>
+  );
+}
