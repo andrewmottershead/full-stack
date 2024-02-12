a89054a7da007e844ce3da2b61df63e12b07b4c0
Andrew Mottersehead <andrew@frenchmottershead.com>
2024-02-12 16:04:35 +0000

    Protecting Routes

--- pages/api/v1/products/[[...id]].js
+++ pages/api/v1/products/[[...id]].js
@@ -1,5 +1,30 @@
-import nc from "next-connect"
-import { getProducts, addProduct, updateProduct, removeProduct } from "@/lib/api-functions/server/products/controllers";
+import nc from "next-connect";
+import {
+    getSession,
+  } from "@auth0/nextjs-auth0";
+
+import {
+    handleUnauthorisedAPICall,
+    checkPermissions,
+    checkRole,
+  } from "@/lib/api-functions/server/utils";
+
+import permissions from "@/lib/api-functions/server/permissions.js"; 
+
+const {
+    identifier,
+    permissions: {
+      products: {
+        create: createProducts,
+        read: readProducts,
+        update: updateProducts,
+        remove: removeProducts,
+      },
+    },
+  } = permissions;
+
+  
+// import { getProducts, addProduct, updateProduct, removeProduct } from "@/lib/api-functions/server/products/controllers";
 
 const baseRoute = "/api/v1/products/:id?";
 
@@ -13,17 +38,40 @@ const handler = nc({
     },
     attachParams: true,
 })
-// .use(someMiddleware())
+.use(async (req, res, next) => {
+    if (req.method === "GET") {
+      return next();
+    }
+    try {
+      const session = await getSession(req, res);
+      req.user = session.user;
+      // console.log(session);
+      console.log(req.user);
+      next();
+    } catch (err) {
+      console.log('err', err);
+      return handleUnauthorisedAPICall(res);
+    }
+  })
 .get(baseRoute, async (req, res) => {
     getProducts(req, res);
 })
 .post(baseRoute, async (req, res) => {
+    if (!checkPermissions(req.user, identifier, createProducts)) {
+        return handleUnauthorisedAPICall(res);
+      }
     addProduct(req, res);
 })
 .put(baseRoute, async (req, res) => {
+    if (!checkPermissions(req.user, identifier, updateProducts)) {
+        return handleUnauthorisedAPICall(res);
+      }
     updateProduct(req, res);
 })
 .delete(baseRoute, async (req, res) => {
+    if (!checkPermissions(req.user, identifier, removeProducts)) {
+        return handleUnauthorisedAPICall(res);
+      }
     removeProduct(req, res);
 });
 
